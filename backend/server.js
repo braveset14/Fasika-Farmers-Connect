08ab293d9f2b457869d7cb2b69362c4aa338ba1a
braveset14 <braveset14@gmail.com>
2026-01-11 00:30:25 +0300

    Refactor CORS middleware and server listen options

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const weatherRoutes = require('./src/routes/weatherRoutes');
 app.use('/api/weather', weatherRoutes);
 // Middleware
 const allowedOrigins = [
-  'http://localhost:5173',  // Local dev
+  'http://localhost:5173',  
   'https://fasika-farmers-connect.onrender.com', 
   'https://fasika-farmers-connect.vercel.app'
 ];
@@ -32,21 +32,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  
-  // Handle preflight
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
-  next();
-});
-app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/products', productRoutes);
@@ -310,7 +295,7 @@ async function startServer() {
     
     // Start server
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0', () => {
       console.log('\n✨ ========================================== ✨');
       console.log('   🚀 Server started successfully!');
       console.log(`   📍 http://localhost:${PORT}`);
