528b2ab76676cb1f556e7f583e173b71ffd4388d
braveset14 <braveset14@gmail.com>
2026-01-11 11:37:19 +0300

    Add updateUserProfile route to userRoutes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getUserProfile } = require('../controllers/userController');
+const { getUserProfile,updateUserProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/profile', protect, getUserProfile);
-
+router.put('/profile', protect, updateUserProfile);  
 module.exports = router;
