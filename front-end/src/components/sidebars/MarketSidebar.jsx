2d8cbaca7486e76712fd725b864f8b4afaad866c
Haimanot515 <haimanotbeka@gmail.com>
2026-01-21 16:10:01 +0300

    Update MarketSidebar.jsx

--- front-end/src/components/sidebars/MarketSidebar.jsx
+++ front-end/src/components/sidebars/MarketSidebar.jsx
@@ -146,8 +146,8 @@ const MarketSidebar = ({ isOpen = true }) => {
                 <div className="l3-container">
                   <NavLink to="/market/sales/add-listing" style={l3LinkStyle}><FaPlusCircle /> <span className="sentence-case">Add new listing</span></NavLink>
                   <NavLink to="/market/sales" style={l3LinkStyle}><FaListUl /> <span className="sentence-case">My active listings</span></NavLink>
-                  <NavLink to="/market/sales/record-transaction" style={l3LinkStyle}><FaExchangeAlt /> <span className="sentence-case">Record transaction</span></NavLink>
-                  <NavLink to="/market/sales/transaction-history" style={l3LinkStyle}><FaHistory /> <span className="sentence-case">Delete All </span></NavLink>
+                  
+                 
                 </div>
               </div>
             )}
